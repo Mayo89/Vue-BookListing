834b12a82293b4d0c5343ee9b863c7fd16372d9f
Steven Mayo <steven.mayo@seabrokers.co.uk>
2020-10-21 13:50:11 +0100

    Module 1.1.1

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
-    <h1>{ title }</h1>
+    <h1>{{ title }}</h1>
   </div>
 
 </template>
 
 <script>
-  export default { name: 'BookList' };
-
-  function data() {
-    return title = "All Books"
-  }
-
+  export default {
+    name: 'BookList',
+    data() {
+      return {
+        title: "All Books"
+      }
+    }
+  };
 </script>
 
 <style>
